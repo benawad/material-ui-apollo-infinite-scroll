cd241cddaf0d865c3b961034c356ff3f1bd07759
Ben Awad <benawad97@gmail.com>
2019-03-26 19:52:27 -0500

    infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { List, ListItem, Paper } from "@material-ui/core";
+import { CircularProgress, List, ListItem, Paper } from "@material-ui/core";
 import React from "react";
+import { Waypoint } from "react-waypoint";
 import { useBooksQuery } from "./generated/ApolloHooks";
 
 const App = () => {
-  const { data } = useBooksQuery({
-    variables: { first: 50 }
+  const { data, fetchMore, networkStatus } = useBooksQuery({
+    variables: { first: 50 },
+    notifyOnNetworkStatusChange: true
   });
 
   if (!data || !data.books) {
-    return <div>...loading</div>;
+    return <CircularProgress />;
   }
 
   return (
@@ -20,10 +22,42 @@ const App = () => {
       <div style={{ maxWidth: 400, margin: "auto", padding: 10 }}>
         <Paper>
           <List>
-            {data.books.books.map(x => (
-              <ListItem key={x.id}>{x.title}</ListItem>
+            {data.books.books.map((x, i) => (
+              <React.Fragment key={x.id}>
+                <ListItem>{x.title}</ListItem>
+                {i === data.books.books.length - 10 && (
+                  <Waypoint
+                    onEnter={() =>
+                      fetchMore({
+                        variables: {
+                          first: 50,
+                          cursor:
+                            data.books.books[data.books.books.length - 1].id
+                        },
+                        updateQuery: (pv, { fetchMoreResult }) => {
+                          if (!fetchMoreResult) {
+                            return pv;
+                          }
+
+                          return {
+                            books: {
+                              __typename: "BooksResponse",
+                              books: [
+                                ...pv.books.books,
+                                ...fetchMoreResult.books.books
+                              ],
+                              hasNextPage: fetchMoreResult.books.hasNextPage
+                            }
+                          };
+                        }
+                      })
+                    }
+                  />
+                )}
+              </React.Fragment>
             ))}
           </List>
+          {networkStatus === 3 && <CircularProgress />}
         </Paper>
       </div>
     </div>
